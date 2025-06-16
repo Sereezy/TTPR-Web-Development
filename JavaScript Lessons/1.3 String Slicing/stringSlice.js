b3a59b12ac752f2acf64b1884429fc5983bcc6a2
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-16 11:22:07 -0400

    string slicing exercise complete

--- JavaScript Lessons/1.3 String Slicing/stringSlice.js
+++ JavaScript Lessons/1.3 String Slicing/stringSlice.js
@@ -3,10 +3,9 @@ Write a function that slices the users input to 140 characters.
 
 Return the sliced input
 */
-
-
-
-
+function sliceString(text){
+    return text.slice(0,140);
+}
 
 /* SAMPLE INPUT */
 
