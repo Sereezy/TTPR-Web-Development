06200de8091ea92f0dd93688cbd9ca924abc1f74
norm miller <norm.miller1@gmail.com>
2025-06-16 11:09:48 -0400

    Save the changes

--- JavaScript Lessons/1variableExercise.js
+++ JavaScript Lessons/1variableExercise.js
@@ -26,9 +26,9 @@ function test() {
     
 /***********Do not change the code above 👆*******/
 //Write your code on lines 29 - 31:
-
-
-    
+var a = "3"
+var b = "8"
+   
 /***********Do not change the code below 👇*******/
 
     console.log("a is " + a);
