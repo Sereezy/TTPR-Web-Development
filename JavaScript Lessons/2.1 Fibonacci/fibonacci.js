96b350200c2d3907c336c24fd5216b00dc766914
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-17 11:21:20 -0400

    finsihed fibonacci exercise

--- JavaScript Lessons/2.1 Fibonacci/fibonacci.js
+++ JavaScript Lessons/2.1 Fibonacci/fibonacci.js
@@ -37,3 +37,21 @@ Do NOT change any of the existing code.
 
 The first two numbers in the sequence must be 0 and 1.
 */
+
+function fibonacciGenerator(n){
+    var output = [];
+    if (n === 1){
+        output = [0];
+    }
+    else if (n === 2){
+        output = [0,1];
+    } else {
+        output = [0,1];
+        for (var i = 2; i < n; i++){
+            output.push(output[output.length - 2] + output[output.length -1]);
+        }
+    }
+    return output;
+}
+
+console.log(fibonacciGenerator(3));
