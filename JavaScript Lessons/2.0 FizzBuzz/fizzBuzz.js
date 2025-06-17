3fcd22705e56388b78ecd5a574f769e913deb919
Maolong Lin <maolong131@gmail.com>
2025-06-17 14:56:34 -0400

    6/17

---
+++ JavaScript Lessons/2.0 FizzBuzz/fizzBuzz.js
@@ -0,0 +1,30 @@
+function fizzBuzz() {
+  let i = 1; // Start counting from 1
+
+  while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      // If divisible by both 3 and 5, print "FizzBuzz"
+      console.log("FizzBuzz");
+    }
+
+    // If divisible only by 3, print "Fizz"
+    else if (i % 3 === 0) {
+      console.log("Fizz");
+    }
+
+    // If divisible only by 5, print "Buzz"
+    else if (i % 5 === 0) {
+      console.log("Buzz");
+    }
+
+    // If divisible by neither, just print the number
+    else {
+      console.log(i);
+    }
+
+    // Move to the next number
+    i++;
+  }
+}
+
+fizzBuzz();
