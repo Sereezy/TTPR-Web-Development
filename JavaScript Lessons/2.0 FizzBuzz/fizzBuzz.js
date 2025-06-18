c62f599b9573d3ca96dfee18a219c0dd1d1f7131
Chrisg3142 <130247951+Chrisg3142@users.noreply.github.com>
2025-06-18 10:12:56 -0400

    adding solutions

--- JavaScript Lessons/2.0 FizzBuzz/fizzBuzz.js
+++ JavaScript Lessons/2.0 FizzBuzz/fizzBuzz.js
@@ -1,12 +1,11 @@
-for (let num = 0; num <= 50; num++){
-    if (num % 3 == 0 && num%5 == 0 && num != 0){
+for (let num = 1; num <= 50; num++){
+    if (num % 3 == 0 && num%5 == 0){
         console.log("fizzBuzz");
-        console.log(num);
-    } else if (num % 3 == 0  && num != 0){
+    } else if (num % 3 == 0){
         console.log("fizz");
-        console.log(num);
-    } else if (num % 5 == 0  && num != 0){
+    } else if (num % 5 == 0){
         console.log("buzz");
+    } else {
         console.log(num);
-    } 
+    }
 }
