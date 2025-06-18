e29fb9b1c44cd17b68893969b0b2db195dfa2971
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-18 13:25:06 -0400

    making alert for all buttons

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -2,8 +2,12 @@ function handleClick() {
     alert("I got clicked!");
 }
 
-document.querySelector("button").addEventListener("click", handleClick);
+// document.querySelector("button").addEventListener("click", handleClick);
 
-document.querySelectorAll("button")[1].addEventListener("click", handleClick);
+// document.querySelectorAll(".drum").addEventListemer("click", handleClick);
 
-
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert("I got clicked!");
+    });
+}
