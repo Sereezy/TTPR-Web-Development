f2292cddd9b51cff34825d81b20b4698c8d1a92b
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-18 13:54:36 -0400

    adding sound to each button, changing background img

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -6,8 +6,11 @@
 
 // document.querySelectorAll(".drum").addEventListemer("click", handleClick);
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
-    });
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var audio = new Audio("sounds/tom-1.mp3")
+    audio.play()
+})
 }
+
+
