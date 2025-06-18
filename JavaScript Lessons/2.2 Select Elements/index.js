c5e6b64d0cdb87401bb751748703fcadbe55aced
elmehdi msiah <natir14mehdi@gmail.com>
2025-06-18 11:18:34 -0400

    wensday changes

--- JavaScript Lessons/2.2 Select Elements/index.js
+++ JavaScript Lessons/2.2 Select Elements/index.js
@@ -5,3 +5,17 @@ Change the text of the title to say Goodbye World
 Change the text inside the anchor tag to be blue
 Change the "Second" list item to be purple
 */
+
+/*
+document.querySelector(".btn").style.backgroundColor = "red";
+document.querySelector(".btn").style.color = "white";
+document.queryselector("h1").innerHTML = "Goodbye world"
+document.querySelector("li a").style.color = "pink";
+document.querySelectorAll("#list .item")[1].style.color = "purple";
+
+*/
+
+// index.js
+function makeHuge() {
+  document.querySelector("h1").classList.add("huge");
+}
