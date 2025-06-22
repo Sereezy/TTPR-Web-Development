662cd0d2f70008cf3077e882a6f5106ba9baff7f
borys solorzano <borys.solo19@gmail.com>
2025-06-22 15:39:11 -0400

    last changes to lesson 1 of js

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -3,7 +3,7 @@
 //   }
 
 //   for (var i =0 ;i < document.querySelectorAll(".drum").length;i++)
-  
+
 //     for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
 //         document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
 //         // alert("I got clicked!");
@@ -11,53 +11,43 @@
 //         )};
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-      const buttonKey = this.innerHTML.toLowerCase();
-      playSound(buttonKey);
-    });
-  }
-  
-  
-  document.addEventListener("keydown", function (event) {
-    const pressedKey = event.key.toLowerCase();
-    playSound(pressedKey);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const buttonKey = this.innerHTML.toLowerCase();
+    playSound(buttonKey);
   });
+}
 
+document.addEventListener("keydown", function (event) {
+  pressedKey = event.key.toLowerCase();
+  playSound(pressedKey);
+});
 
-function playSound(key){
+function playSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+        new Audio("sounds/kick-bass.mp3").play();
+      break;
+    case "k":
 
-switch(key)
-{
-case "w":
-
-new Audio ("sounds/tom-1.mp3").play();
-break;
-case "a":
-
-new Audio ("sounds/tom-2.mp3").play();
-break;
-case "s":
-
-new Audio ("sounds/tom-3.mp3").play();
-break
-case "d":
-
-new Audio ("sounds/tom-4.mp3").play();
-break;
-case "j":
-
-new Audio ("sounds/crash.mp3").play();
-break;
-case "k":
-
-new Audio ("sounds/kick-bass.mp3").play();
-break;
-case "l":
-
-new Audio ("sounds/snare.mp3").play();
-break;
-default:
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    default:
 
     // console.log(key)
-
-}}
+  }
+}
