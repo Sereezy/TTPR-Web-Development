6487d7671787f32b520a816cec9978fc874d6ad4
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-06-23 10:44:18 -0400

    Complete JS 2.4

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -4,27 +4,44 @@ let instrument = document.querySelectorAll("button");
 for (var i = 0; i < instrument.length; i++){
     instrument[i].addEventListener("click", function(){
 
-        //var note = instrument[i].innerHTML;
+        var note = this.innerHTML;
         //console.log(note);
 
-        var audio = new Audio("sounds/tom-1.mp3")
-/*
-        if (note == "w"){
-            audio = new Audio("sounds/crash.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/kick.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/snare.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/tom-1.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/tom-2.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/tom-3.mp3")
-        } else if (note == "a"){
-            audio = new Audio("sounds/tom-4.mp3")
+        
+        switch (note){
+            case "w":
+                audio = new Audio("sounds/crash.mp3");
+                break;
+
+            case "a":
+                audio = new Audio("sounds/kick-bass.mp3");
+                break;
+
+            case "s":
+                audio = new Audio("sounds/snare.mp3");
+                break;
+
+            case "d":
+                audio = new Audio("sounds/tom-1.mp3");
+                break;
+
+            case "j":
+                audio = new Audio("sounds/tom-2.mp3");
+                break;
+
+            case "k":
+                audio = new Audio("sounds/tom-3.mp3");
+                break;
+
+            case "l":
+                audio = new Audio("sounds/tom-4.mp3");
+                break;
+
+            default:
+                audio = new Audio("sounds/tom-1.mp3");
+                break;
         }
-            */
+            
         audio.play();
     })
 }
