ea1141efbefb13345330e9e888d8a5eec2a43b9e
Angel Solano <angels31927@gmail.com>
2025-06-23 10:50:56 -0400

    im lost asf

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -1,6 +1,25 @@
-
 document.querySelectorAll('drum').forEach(element => {
     element.addEventListener('click', function(){
-        alert('I got clicked!')
+        this.style.Color = 'orange';
+        let key = this.className.split(' ')[0];
+        function alertKey(string){
+            alert(`${string}-key was pressed!`)
+        }
+        switch (situation){
+            case 'w':
+                alertKey('w')
+            case 'a':
+                alertKey('a')
+            case 's':
+                alertKey('s')
+            case 'd':
+                alertKey('d')
+            case 'j':
+                alertKey('j')
+            case 'k':
+                alertKey('k')
+            case 'l':
+                alertKey('l')
+        }
     })
 })
