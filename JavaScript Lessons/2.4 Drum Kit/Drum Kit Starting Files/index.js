b3ec3c21bb7517666e019f870198a05e78bb35e8
Sutibro Barua <boyback485@gmail.com>
2025-06-23 11:33:59 -0400

    changed from keypress to keydown

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/index.js
@@ -14,7 +14,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 }
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keydown", function(event) {
+
 
   makeSound(event.key);
 
