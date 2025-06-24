b6c6bbeb71748145e5ce900215ebf29e45191d3d
Nazia Mahmud <nazia.mahmud70@gmail.com>
2025-06-24 10:02:13 -0400

    changes

---
+++ JavaScript Lessons/Challange/index.js
@@ -0,0 +1,15 @@
+function Chef(name, title, specialty, yearsExperience) {
+  this.name = name;
+  this.title = title;
+  this.specialty = specialty;
+  this.yearsExperience = yearsExperience;
+
+  this.cookDish=function (dish){
+ console.log(`As a ${this.title}, I'm cooking up a delicious ${dish}!`)
+  } 
+
+  
+}
+
+var chef1 = new Chef("Lucia", "Sous Chef", "Italian Cuisine", 8)
+chef1.cookDish("lasagna");
