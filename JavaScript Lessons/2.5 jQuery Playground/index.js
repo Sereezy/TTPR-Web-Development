a99c2593525996270bfa44c09790f2e5d4f402fa
Serena Burton <SerenaBurton@yahoo.com>
2025-06-24 09:47:41 -0400

    commented out more code

--- JavaScript Lessons/2.5 jQuery Playground/index.js
+++ JavaScript Lessons/2.5 jQuery Playground/index.js
@@ -2,60 +2,60 @@
 console.log($("button"));
 
 // Get the current CSS value by passing the property name as a string
-console.log($("h1").css("font-size"));
+// console.log($("h1").css("font-size"));
 
 //Manipulating Styles
-$("h1").css("color", "darkseagreen");
+// $("h1").css("color", "darkseagreen");
 
 //Add Class
-$("h1").addClass("big-title");
+// $("h1").addClass("big-title");
 
 //Remove Class
-$("h1").removeClass("big-title");
+// $("h1").removeClass("big-title");
 
 //Add Multiple Classes
-$("h1").addClass("big-title margin-50");
+// $("h1").addClass("big-title margin-50");
 
 //Remove Multiple Classes
-$("h1").removeClass("big-title margin-50");
+// $("h1").removeClass("big-title margin-50");
 
 //.hasClass
-console.log($("h1").hasClass());
+// console.log($("h1").hasClass());
 
 //Add text with element.text('newText')
-$("button").text("Press");
+// $("button").text("Press");
 
 //Add text w/ html tags using element.html('<tag>newText</tag>')
-$("button").eq(1).html("<strong>Pressed</strong>");
+// $("button").eq(1).html("<strong>Pressed</strong>");
 
 //Getting attribute from anchor tag
-console.log($("a").attr("href"));
+// console.log($("a").attr("href"));
 
 //Setting attribute to link to Netflix
-console.log($("a").attr("href", "https://www.netflix.com/"));
+// console.log($("a").attr("href", "https://www.netflix.com/"));
 
 //Getting classes from h1 tag
-console.log($("h1").attr("class"));
+// console.log($("h1").attr("class"));
 
 //Adding .click() eventListener to h1
-$("h1").click(function () {
-  $("h1").css("color", "purple");
-});
+// $("h1").click(function () {
+//   $("h1").css("color", "purple");
+// });
 
 //Adding click event listener to multiple elements
-$("button").click(function () {
-  $("button").text("clicked");
-});
+// $("button").click(function () {
+//   $("button").text("clicked");
+// });
 
 //Replacing h1 text with the key pressed using keydown
-$(document).keypress(function (event) {
-  $("h1").text(event.key);
-});
+// $(document).keypress(function (event) {
+//   $("h1").text(event.key);
+// });
 
 //Using .on() to detect a mouseover event on the h1 element and change its color to purple
-$("h1").on("mouseover", function () {
-  $("h1").css("color", "pink");
-});
+// $("h1").on("mouseover", function () {
+//   $("h1").css("color", "pink");
+// });
 
 //<buttonTagBefore> <h1>hello</h1>
 // $('h1').before('<button>before</button>')
@@ -70,29 +70,29 @@ $("h1").on("mouseover", function () {
 // $('h1').append('<button>append</button>')
 
 //toggle jQuery animations
-$("button").on("click", function () {
-  $("h1").toggle();
-});
+// $("button").on("click", function () {
+//   $("h1").toggle();
+// });
 
 //toggle jQuery animations
-$("button").on("click", function () {
-  $("h1").fadeToggle();
-});
+// $("button").on("click", function () {
+//   $("h1").fadeToggle();
+// });
 
 //toggle jQuery animations
-$("button").on("click", function () {
-  $("h1").slideToggle();
-});
+// $("button").on("click", function () {
+//   $("h1").slideToggle();
+// });
 
 //toggle jQuery animations (only for numerical values)
-$("button").on("click", function () {
-  $("h1").animate({fontSize: 90});
-});
+// $("button").on("click", function () {
+//   $("h1").animate({fontSize: 90});
+// });
 
 //chaining jQuery animations together
-$("button").on("click", function () {
-  $("h1").slideUp().slideDown().hide().show().animate({opacity: .5});
-});
+// $("button").on("click", function () {
+//   $("h1").slideUp().slideDown().hide().show().animate({opacity: .5});
+// });
 
 
 // 1. Change the text color of the <h1> to 'teal'.
