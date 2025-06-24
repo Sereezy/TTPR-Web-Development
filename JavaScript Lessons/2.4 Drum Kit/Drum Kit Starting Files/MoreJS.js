57da0b1ddcef704633b9f09c014c570f13bc6e06
Syed Asghar <syedasghar178@gmail.com>
2025-06-24 19:42:35 -0400

    answering 2.4

--- JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/MoreJS.js
+++ JavaScript Lessons/2.4 Drum Kit/Drum Kit Starting Files/MoreJS.js
@@ -8,17 +8,92 @@ function Chef (name, title, specialty, yearsExperience) {
 }
 
 var chef1 = new Chef("Lucia", "Sous Chef", "Italian Cuisine", 8);
-chef1.cookDish("lasagna")
+chef1.cookDish("lasagna");
 
 
+/*
+document.addEventListener("keydown", 
+    function(event){
+        console.log(event)
+})
+*/
 
 
-function Audio(fileLocation) {
-    this.fileLocation = fileLocation;
-    this.play = function() {
-
+function makeSound(key){
+    switch (key) {
+    case "w":
+        var w = new Audio("sounds/crash.mp3")
+        w.play()
+        break;
+    case "a":
+        var a = new Audio("sounds/kick-bass.mp3")
+        a.play()
+        break;
+    case "s":
+        var s = new Audio("sounds/snare.mp3")
+        s.play()
+        break;
+    case "d":
+        var d = new Audio("sounds/tom-1.mp3")
+        d.play()
+        break;
+    case "j":
+        var j = new Audio("sounds/tom-2.mp3")
+        j.play()
+        break;
+    case "k":
+        var k = new Audio("sounds/tom-3.mp3")
+        k.play()
+        break;
+    case "l":
+        var l = new Audio("sounds/tom-4.mp3")
+        l.play()
+        break;
+    deafult:
+        console.log(button)
     }
 }
 
-var tom1 = new Audio("./sounds/tom-1.mp3")
-    tom1.play();
+
+for (i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+            var button = this.innerHTML
+            makeSound(button)
+        })
+}
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    console.log(event.key);
+})
+
+/*
+for (i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        this.classList.add("pressed")
+    })
+}
+*/
+
+
+function buttonPressed (press){
+    var selected = document.querySelector("." + press);
+    selected.classList.add("pressed");
+
+    setTimeout(function() {
+        selected.classList.remove("pressed");
+    }, 100);
+}
+
+
+document.addEventListener("keydown", function(event){
+    buttonPressed(event.key);
+})
+
+
+/*
+setTimeout(funtion() {
+
+}, 100);
+;
+*/
