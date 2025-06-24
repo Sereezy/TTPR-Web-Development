29f0d54f1afed0c8101c41c4272d4a5a7ad4a163
Sutibro Barua <boyback485@gmail.com>
2025-06-24 13:42:33 -0400

    great

--- JavaScript Lessons/2.6 Simon Game/Simon Game Challenge Starting Files/game.js
+++ JavaScript Lessons/2.6 Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,35 +1,83 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click(function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    
-    playSound(userChosenColour);
+var level = 0;
+var started = false;
+
+// Detect button click
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      // If they finished the full sequence, go to next level
+      if (userClickedPattern.length === gamePattern.length) {
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      // ❌ Wrong answer
+      playSound("wrong");
   
-  });
+      $("body").addClass("game-over");
+  
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+  
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+  
+      // Reset the game
+      startOver();
+    }
+  }
   
 
-// 3. Create the nextSequence function
+// Detect keypress to start game
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// Generate next sequence
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
 }
 
+// Play sound helper
 function playSound(name) {
-
-var audio = new Audio("Sounds/" + name + ".mp3");
-audio.play();
-
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
+// Animate press helper
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
 
 
 
