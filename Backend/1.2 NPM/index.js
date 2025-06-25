b0aeebb05aa52fe8cb48ace36e7495b5290e443d
Sahil Singh <Sahil18adil@gmail.com>
2025-06-25 13:28:36 -0400

    superheroes and silly name

---
+++ Backend/1.2 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName from "sillyname";
+var generateName = require('sillyname');
+var sillyName = generateName();
+console.log(sillyName)
+
+import {randomSuperhero} from "superheroes";
+const name = randomSuperhero()
+console.log(name)
