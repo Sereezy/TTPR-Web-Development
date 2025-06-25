f1ba640d71f8a8f2f466e6db63d482d0afbf8c9b
Sutibro Barua <boyback485@gmail.com>
2025-06-25 13:31:58 -0400

    good

--- Backend/1.2 NPM/index.js
+++ Backend/1.2 NPM/index.js
@@ -1,8 +1,8 @@
 import generateName from "sillyname";
 var generateName = require('sillyname');
 var sillyName = generateName();
-console.log(sillyName)
+console.log(`I am ${sillyName}!`);
 
 import {randomSuperhero} from "superheroes";
-const name = randomSuperhero()
-console.log(name)
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
