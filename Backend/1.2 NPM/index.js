d3030cee3bf13b46f4b73039f5b2a61c81c4a1a9
Daniela Sanchez <danielasaledesma14@gmail.com>
2025-06-25 13:55:28 -0400

    Wed2 Push

---
+++ Backend/1.2 NPM/index.js
@@ -0,0 +1,7 @@
+import generateNane from "sillyname";
+var sillyName = generateNane();
+console.log(sillyName)
+
+import { randomSuperhero  } from "superheroes";
+const name = randomSuperhero() 
+console.log(name)
