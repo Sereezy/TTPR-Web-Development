217dfd8a36c160bf614d31d96dacb95247b99583
Sahil Singh <Sahil18adil@gmail.com>
2025-06-25 13:51:01 -0400

    inquirer

--- Backend/1.2 NPM/index.js
+++ Backend/1.2 NPM/index.js
@@ -1,8 +1,9 @@
-import generateName from "sillyname";
-var generateName = require('sillyname');
-var sillyName = generateName();
-console.log(sillyName)
 
-import {randomSuperhero} from "superheroes";
-const name = randomSuperhero()
-console.log(name)
+import generateName from "sillyname";
+const sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
