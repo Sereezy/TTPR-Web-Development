c5d9e8fb67be48e669e38ef72ab3298a8f976a8f
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-30 11:42:27 -0400

    testing methods  using postman

--- Backend/1.5 Express Server/index.js
+++ Backend/1.5 Express Server/index.js
@@ -10,3 +10,34 @@ app.get ("/", function(req, res){
     console.log(req.rawHeaders);
     res.send("Hello World :]");
 });
+
+app.get ("/contact", function(req, res){
+    res.send("<p>HI this is contact endpoint</p>");
+});
+
+app.get ("/about", function(req, res){
+    res.send("<h1>ANNNNND this is the about endpoint hehe</h1>");
+});
+
+
+app.post ("/register", (req, res) => {
+    // if (!req.body.email){
+    //     return res.status(400).send("Email is required");
+    // }
+    res.status(201).send("User created");
+});
+
+app.put ("/puttest", (req, res) => {
+    res.status(200).send("OK - put");
+});
+
+app.patch ("/patchtest", (req, res) => {
+    res.status(200).send("OK - patch");
+});
+
+app.delete ("/deletetest", (req, res) => {
+    res.status(200).send("OK - delete");
+});
+
+// res.status(404).send("Page not found");
+// res.sendStatus(404);
