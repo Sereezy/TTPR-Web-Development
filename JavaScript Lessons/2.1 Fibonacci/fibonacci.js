592341957117c187e957d122aaf12d0bd9fe340c
Kelline Lubrun <kslubrun@gmail.com>
2025-06-30 11:05:09 -0400

    saving local changes before pulling

--- JavaScript Lessons/2.1 Fibonacci/fibonacci.js
+++ JavaScript Lessons/2.1 Fibonacci/fibonacci.js
@@ -37,3 +37,21 @@ Do NOT change any of the existing code.
 
 The first two numbers in the sequence must be 0 and 1.
 */
+
+function fibonacciGenerator(n) {
+    let output = [];
+  
+    if (n === 1) {
+      output = [0];
+    } else if (n === 2) {
+      output = [0, 1];
+    } else {
+      output = [0, 1];
+      for (let i = 2; i < n; i++) {
+        output.push(output[i - 1] + output[i - 2]);
+      }
+    }
+  
+    return output;
+  }
+  
