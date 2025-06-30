74c978a951732077f25d52b53b48200c73cb6903
Zohinur-Miah <zohinur.m17@gmail.com>
2025-06-30 08:38:48 -0700

    local host

--- Backend/1.5 Express server/index.js
+++ Backend/1.5 Express server/index.js
@@ -4,3 +4,14 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+ app.get("/", function (req, res) {
+  res.send("Hello World!");
+ });
+
+ app.get("/", function(req, res) {
+	//log to see request info
+    console.log(req)
+    console.log(req.rawHeaders) //key,value that tell you about where the request originated
+//res.send() html snippet
+res.send("Hello World");
+});
