f5394f4d3e8451157e93644d2312894d93b7f925
Daniela Sanchez <danielasaledesma14@gmail.com>
2025-06-30 14:37:22 -0400

    Three push

--- Backend/1.7 Middleware/index1.js
+++ Backend/1.7 Middleware/index1.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {bodyParser
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
