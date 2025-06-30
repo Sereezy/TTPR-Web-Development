33a674a4662190f71d6d70ba5c3de601a6a48cdd
Arisa Moonsammy <aricollegem@gmail.com>
2025-06-30 13:10:30 -0400

    submit endpoint with form

--- Backend/1.7 Middleware/index1.js
+++ Backend/1.7 Middleware/index1.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -6,8 +7,17 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
+  console.log(__dirname + "/public/index.html") //log path
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("Form data received successfully.");
 });
 
 app.listen(port, () => {
