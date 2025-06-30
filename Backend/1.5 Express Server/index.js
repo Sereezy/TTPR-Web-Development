d3ab96d078193c610d618f47b646ee74b1059581
Anna Rose Cimitile <annarosecimitile@gmail.com>
2025-06-30 11:40:28 -0400

    practicing express

--- Backend/1.5 Express Server/index.js
+++ Backend/1.5 Express Server/index.js
@@ -4,6 +4,18 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", function(req, res){
+    res.send("Hello World")
+});
+
+app.get("/contact", function(req,res){
+    res.send("This is a contact page")
+});
+
+app.get("/about", function(req,res){
+    res.send("This is an about page")
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}. `);
 });
