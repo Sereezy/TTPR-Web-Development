67fa05c2efaa4be948eeba1b5316f99719338414
India Barker <india.barker@mail.citytech.cuny.edu>
2025-07-01 13:27:50 -0400

    New changes

--- Backend/1.5 Express Server/index.js
+++ Backend/1.5 Express Server/index.js
@@ -2,11 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
-});
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     console.log(req)
     console.log(req.rawHeaders)
   res.send("Hello, World!");
@@ -18,5 +15,9 @@ app.get("/about", function(req, res) {
 
 app.get("/contact", function(req, res) {
   res.send("<h1>Contact Me</h1><p>3225674322</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
 
