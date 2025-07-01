ee7b0291531cd4415a24e131f1b281a914e4731d
Arisa Moonsammy <aricollegem@gmail.com>
2025-07-01 10:50:25 -0400

    added res send

--- Backend/1.7 Middleware/index1.js
+++ Backend/1.7 Middleware/index1.js
@@ -15,7 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
+  res.send("Form data received!");
 })
 
 app.listen(port, () => {
