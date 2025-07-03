01963c32fb865fea4600c86f722b2d492d67f9f8
Arisa Moonsammy <aricollegem@gmail.com>
2025-07-03 11:35:40 -0400

    index4.js complete

--- Backend/1.7 Middleware/index4.js
+++ Backend/1.7 Middleware/index4.js
@@ -1,9 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
-import { fileURLToPath } from "url;"
+import { fileURLToPath } from "url";
 
-const __dirname = direname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 var bandName = "";
@@ -19,11 +19,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.unsubscribe(bandNameGenerator)
+app.use(bandNameGenerator)
 
 app.post("/submit", (req, res) => {
   res.send(`<h1>Your band name is :</h1><h2>${bandName}</h2>`);
-});
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
