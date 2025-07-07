b3e814872ed4311be9a8571f71c4775c0f9842d0
Angel Solano <angels31927@gmail.com>
2025-07-07 10:49:24 -0400

    ok

--- Backend/1.7 Middleware/index2.js
+++ Backend/1.7 Middleware/index2.js
@@ -1,10 +1,17 @@
 import express from "express";
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
+
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.post('/submit', (req, res) => {
+  res.json({message: 'Post received'})
 });
 
 app.listen(port, () => {
