b44af2e89e3ae2e2a4a52a002d77f19081f867b5
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-07-08 11:19:40 -0400

    Update Backend 1.7-1.8

--- Backend/1.8 Secrets Project/index.js
+++ Backend/1.8 Secrets Project/index.js
@@ -1,3 +1,48 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+let correctPassword = "ILoveProgramming";
+let verified = false;
+
+function passwordCheck(req, res, next){
+    const {password} = req.body;
+    
+    //console.log({"password" : password});
+
+    if (password === correctPassword){
+        verified = true;
+    } else {
+        verified = false;
+    }
+    next();
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+  //res.sendStatus(201);
+})
+
+app.post("/check", passwordCheck, (req, res) => {
+  //res.send(`<h1>Band Name: ${bandName}</h1>`);
+  if (verified){
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
