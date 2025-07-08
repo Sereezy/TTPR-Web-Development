b53bc9887e7a34f2cdb23505705cbe91e26cef0f
Ishua-Belzince <ishua.belzince@outlook.com>
2025-07-08 11:40:08 -0400

    changes

--- Backend/1.7 Middleware/index3.js
+++ Backend/1.7 Middleware/index3.js
@@ -1,18 +1,22 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 const port = 3000;
 
 
-const logger = (req, res, next) => {
-  console.log("Request Method:", req.method);
-  console.log("Request URL:", req.url);
+function logger(req, res, next) {
+  console.log(`Method: ${req.method} | URL: ${req.url}`);
   next(); 
-};
+}
+
+
 app.use(logger);
 
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => {
+  res.send("Hello from index3.js!");
+});
 
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
--- Backend/1.8 Secrets Project/index.js
+++ Backend/1.8 Secrets Project/index.js
@@ -1,3 +1,46 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    req.isAuthorized = true;
+  } else {
+    req.isAuthorized = false;
+  }
+  next();
+}
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", passwordCheck, (req, res) => {
+  if (req.isAuthorized) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/"); 
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
