f49ec5ae57a6241c410ef93f29d8d9fe664f9367
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-10 11:17:56 -0400

    removed nodemodules and add solutions

--- Backend/2.1 Passing Data Zip/index.js
+++ Backend/2.1 Passing Data Zip/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 const app = express();
 const port = 3000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  let h1 = "<h1>Enter your name Below</h1>"
+  res.render("index.ejs", { data: h1 });
+});
 
-app.get("/", (req, res) => {});
-
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let name = req.body["fName"] + req.body["lName"];
+  let result = `<h1> Your name has ${name.length} letter in it.</h1>`
+  res.render("index.ejs", { data: result });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
