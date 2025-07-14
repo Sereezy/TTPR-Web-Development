1f5af0d2cb672ac008fb82b8ef201e2664b6adb8
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-07-14 11:05:35 -0400

    Update Backend

--- Backend/2.3 Band Generator Project/index.js
+++ Backend/2.3 Band Generator Project/index.js
@@ -9,6 +9,8 @@ const port = 3000;
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
+app.use(express.static("public"));
+
 //Step 4 - Add a dynamic year to the footer in footer.ejs.
 //Hint: Google to find out how to get the current year using JS.
 
@@ -16,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
@@ -24,7 +27,10 @@ app.post("/submit", (req, res) => {
   //Then:
   //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
   //scroll down to see the two arrays.
+  var randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  var randomNoun = adj[Math.floor(Math.random() * noun.length)];
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
+  res.render("index.ejs", {adj:randomAdj , noun:randomNoun});
   //3. Test to make sure that the random words display in the h1 element in index.ejs
 });
 
