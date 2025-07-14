84dbb976478d0491d1e27c0649546d1d891ccf32
Anna Rose Cimitile <annarosecimitile@gmail.com>
2025-07-14 10:14:06 -0400

    adjusted file

--- Mock Interviews/index.js
+++
@@ -1,8 +0,0 @@
-//Problem: reverse the order in a linked list 
-
-//First, create a function
-function reverseOrder(arr) {
-    var numbers = [0];
-}
-// Creating a for loop to iterate into each part of the list 
-for (let )
