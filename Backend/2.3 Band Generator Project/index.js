60d9084cb79b86afabae618cd48fcc245ba8fd01
India Barker <india.barker@mail.citytech.cuny.edu>
2025-07-14 11:10:05 -0400

    New changes

--- Backend/2.3 Band Generator Project/index.js
+++ Backend/2.3 Band Generator Project/index.js
@@ -9,21 +9,35 @@ const port = 3000;
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
+
+
+
+
+
+
 //Step 4 - Add a dynamic year to the footer in footer.ejs.
 //Hint: Google to find out how to get the current year using JS.
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//Step 1 - Make the get route work and render the index.ejs file.
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("solution.ejs");
 });
 
 app.post("/submit", (req, res) => {
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
+const randomAdj = adj[Math.floor(Math.random() * adj.length)];
   //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
   //scroll down to see the two arrays.
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  res.render("solution.ejs", {
+    adjective: randomAdj,
+    noun: randomNoun,
+  });
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
 });
