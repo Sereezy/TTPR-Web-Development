219021a074ab329e42a3b07f0d84dd913b9d7714
nadia-khan <knadia258@gmail.com>
2025-07-21 10:00:14 -0400

    COMMITING

--- Backend/2.3 Band Generator Project/index.js
+++ Backend/2.3 Band Generator Project/index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
@@ -16,9 +17,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render('index.ejs');
 });
 
 app.post("/submit", (req, res) => {
+  const randomAdj = Math.floor(Math.random() * adj.length);
+  const randomNoun = Math.floor(Math.random() * noun.length);
+  res.render('index.ejs',{  
+  adjective: randomAdj,
+  noun: randomNoun,
+  });
+
+});
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
@@ -26,7 +36,7 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
