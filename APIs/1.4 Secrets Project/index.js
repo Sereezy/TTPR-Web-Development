6908edffe647aeb99465427d2bb414256bd332e9
William Wade <williamwadeisfocused@gmail.com>
2025-07-23 11:37:40 -0400

    ..

--- APIs/1.4 Secrets Project/index.js
+++ APIs/1.4 Secrets Project/index.js
@@ -2,43 +2,30 @@
 // 1. Import express and axios
 import express from "express";
 import axios from "axios";
-import bodyParser from "body-parser";
 // 2. Create an express app and set the port number.
 
 const app = express();
 const port = 3000;
-const API_URL = "https://secrets-api.appbrewery.com";
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // 3. Use the public folder for static files.
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 
 app.get("/", async (req, res) => {
-  try {
-    const result = await axios.get(API_URL + "/random");
-    console.log(result.data)
-    res.render("index.ejs", { content: JSON.stringify(result.data), secret: content.secret, user: content.username });
+    try {
+    const Result = await axios.get("https://secrets-api.appbrewery.com/random");
+    const Data = JSON.parse(JSON.stringify(Result.data))
+    res.render("index.ejs", { secret: Data.secret, user: Data.username });
   } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(result.data), secret: content.secret, user: content.name });
+    res.render("index.ejs", { content: ""});
+    res.status(500)
   }
 });
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
-
-app.post("/", async (req, res) => {
-  console.log(req.body)
-  try {
-    const result = await axios.get(API_URL + "/random");
-    console.log(result.data)
-    res.render("index.ejs", { content: JSON.stringify(result.data), secret: content.secret, user: content.name });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(result.data), secret: content.secret, user: content.name });
-  }
-});
 
 
 // 6. Listen on your predefined port and start the server.
